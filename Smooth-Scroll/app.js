5d642a9746f1c24cec6cb5f32a7d982b3645c5f7
Mack <mackparker001@gmail.com>
2021-01-20 21:15:45 +0530

    smooth

---
+++ Smooth-Scroll/app.js
@@ -0,0 +1,21 @@
+
+// $('.navbar a').on('click', function (e) {
+//   if (this.hash !== '') {
+//     e.preventDefault();
+
+//     const hash = this.hash;
+
+//     $('html, body')
+//       .animate({
+//         scrollTop: $(hash).offset().top
+//       },800);
+//   }
+// });
+
+
+
+
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 800
+});
